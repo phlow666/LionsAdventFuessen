9d5f0192262d879b25aa326bf69833a704ceee1f
Florian Geisenhof <florian.geisenhof@bihler.com>
2018-12-03 10:12:17 +0100

    Refactoring

--- src/Program.cs
+++ src/Program.cs
@@ -8,15 +8,7 @@ namespace lions
     {
         static void Main(string[] args)
         {
-            AdventParser parser = null;
-            if (args == null || args.Length == 0)
-            {
-                parser = new AdventParser("https://www.lions.de/web/lc-fuessen/gewinner-2017", new[] { "174", "0174", "3104" });
-            }
-            else
-            {
-                parser = new AdventParser("https://www.lions.de/web/lc-fuessen/gewinner-2017", args);
-            }
+            var parser = new AdventParser("https://www.lions.de/web/lc-fuessen/gewinner-2017", args);
 
             parser.Parse();
             Console.ReadKey();
