9664a2efeb43c2ae655535f0294debd16d270708
Florian Geisenhof <florian.geisenhof@gmail.com>
2018-12-04 13:26:28 +0100

    Params added to main

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Net.Http;
 
@@ -6,7 +6,7 @@ namespace lions
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(params string[] args)
         {
             var parser = new AdventParser("https://www.lions.de/web/lc-fuessen/gewinner-2017", args);
 
