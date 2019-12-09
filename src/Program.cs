5216f195c912d090f5cc48297754a6b4a4e0dace
Florian Geisenhof <florian.geisenhof@gmail.com>
2019-12-09 06:31:42 +0100

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,7 @@ namespace lions
     {
         static void Main(params string[] args)
         {
-            var parser = new AdventParser("https://www.lions.de/web/lc-fuessen/gewinner-2018", args);
+            var parser = new AdventParser("https://www.lions.de/web/lc-fuessen/gewinner-2019", args);
 
             parser.Parse();
             Console.ReadKey();
